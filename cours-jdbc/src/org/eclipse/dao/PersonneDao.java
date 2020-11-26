b7484b9478bc39831b662cbca37820c3ddb20cff
Moez <moezgrami77@gmail.com>
2020-11-26 16:47:22 +0100

    Jdbc

--- cours-jdbc/src/org/eclipse/dao/PersonneDao.java
+++ cours-jdbc/src/org/eclipse/dao/PersonneDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.config.MyConnection;
 import org.eclipse.model.Personne;
@@ -14,7 +15,7 @@ public class PersonneDao implements Dao<Personne> {
 		Connection c = MyConnection.getConnection();
 		if (c != null) {
 			try {
-				PreparedStatement ps = c.prepareStatement("insert into personne (nom,prenom) values (?,?); ",
+				PreparedStatement ps = c.prepareStatement("INSERT into personne (nom,prenom) values (?,?); ",
 						PreparedStatement.RETURN_GENERATED_KEYS);
 				ps.setString(1, personne.getNom());
 				ps.setString(2, personne.getPrenom());
@@ -29,15 +30,14 @@ public class PersonneDao implements Dao<Personne> {
 			}
 		}
 		return null;
-	} 
+	}
 
-	
 	@Override
 	public void remove(Personne personne) {
 		Connection c = MyConnection.getConnection();
 		if (c != null) {
 			try {
-				PreparedStatement ps = c.prepareStatement("DELETE FROM  personne  WHERE num = ? ; ");
+				PreparedStatement ps = c.prepareStatement("DELETE FROM personne WHERE num = ? ; ");
 				ps.setInt(1, personne.getNum());
 				ps.executeUpdate();
 			} catch (SQLException e) {
@@ -46,21 +46,93 @@ public class PersonneDao implements Dao<Personne> {
 		}
 	}
 
-	// TODO Auto-generated method stub } @Override
-	public Personne update(Personne t) {
-		// TODO Auto-generated method stub
+	@Override
+	public Personne update(Personne personne) {
+		Connection c = MyConnection.getConnection();
+		if (c != null) {
+			try {
+				PreparedStatement ps = c.prepareStatement("UPDATE personne SET nom=? , prenom=? WHERE num=? ; ",
+						PreparedStatement.RETURN_GENERATED_KEYS);
+				ps.setString(1, personne.getNom());
+				ps.setString(2, personne.getPrenom());
+				ps.setInt(3, personne.getNum());
+				int nbr = ps.executeUpdate();
+				if (nbr != 0) {
+					return personne;
+				}			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 
+
 	@Override
 	public Personne findById(int id) {
-		// TODO Auto-generated method stub
+		Connection c = MyConnection.getConnection();
+		if (c != null) {
+			try {
+				PreparedStatement ps = c.prepareStatement("SELECT * FROM Personne WHERE num = ?;");
+				ps.setInt(1, id);
+				ResultSet result = ps.executeQuery();
+				if (result.next()) {
+					int num = result.getInt(1);
+					String nom = result.getString(2);
+					String prenom = result.getString(3);
+					Personne personne = new Personne(num, nom, prenom);
+					return personne;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public List<Personne> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Personne> personnes = new ArrayList<Personne>();
+		Connection c = MyConnection.getConnection();
+		if (c != null) {
+			try {
+				PreparedStatement ps = c.prepareStatement("SELECT * FROM Personne;");
+				ResultSet result = ps.executeQuery();
+				while (result.next()) {
+					int num = result.getInt(1);
+					String nom = result.getString(2);
+					String prenom = result.getString(3);
+					Personne personne = new Personne(num, nom, prenom);
+					personnes.add(personne);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return personnes;
 	}
-}
+	
+	public ArrayList<Personne> findByNomAndPrenom(String nom, String prenom) {
+		ArrayList<Personne> personnes = new ArrayList<Personne>();
+		Connection c = MyConnection.getConnection();
+		if (c != null) {
+			try {
+				PreparedStatement ps = c.prepareStatement("SELECT * FROM Personne WHERE nom = ? AND prenom = ?;");
+				ps.setString(1, nom);
+				ps.setString(2, prenom);				
+				ResultSet result = ps.executeQuery();
+				while (result.next()) {
+					int num = result.getInt(1);
+					String n = result.getString(2);
+					String p = result.getString(3);
+					Personne personne = new Personne(num, n, p);
+					personnes.add(personne);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return personnes;
+	}
+				
+
+}	
--- cours-jdbc/src/org/eclipse/main/Main.java
+++ cours-jdbc/src/org/eclipse/main/Main.java
@@ -1,17 +1,103 @@
 package org.eclipse.main;
 
 import org.eclipse.dao.PersonneDao;
+import org.eclipse.dao.VehiculeDao;
 import org.eclipse.model.Personne;
+import org.eclipse.model.Vehicule;
+import org.eclipse.service.PersonneService;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {  // ewception ici vient d'être ajouter pour remove car on a fait exception sans try
 		// Personne personne = new Personne("thauvin", "florian");
-		PersonneDao personneDao = new PersonneDao();
+		//PersonneDao personneDao = new PersonneDao();
 //		Personne insertedPersonne = personneDao.save(personne);
 //		if (insertedPersonne != null) {
 //			System.out.println(insertedPersonne);
 //		}
-		Personne personne = new Personne(9);
-		personneDao.remove(personne);
+		//Personne personne = new Personne(9);
+		//personneDao.remove(personne);
+		
+		//update:
+		/*
+		 * Personne personne = new Personne(5, "Maradona", "Diego"); PersonneDao
+		 * personneDao = new PersonneDao();
+		 * System.out.println(personneDao.update(personne));
+		 */
+		
+		//findall :
+		/*
+		 * PersonneDao personneDao = new PersonneDao(); var personnes =
+		 * personneDao.findAll(); for(Personne personne: personnes) {
+		 * System.out.println(personne); }
+		 */
+		/*
+		 * PersonneDao personneDao = new PersonneDao();
+		 * System.out.println(personneDao.findById(1));
+		 * System.out.println(personneDao.findById(2));
+		 */
+		
+		//vehicule dao:
+//		PersonneDao personneDao = new PersonneDao();
+//		VehiculeDao  vehiculeDao = new VehiculeDao();
+//		Vehicule vehicule = new Vehicule("Peugeot", "108", 2018, personneDao.findById(5));
+//		System.out.println(vehiculeDao.save(vehicule));
+		
+		//vehicule remove:
+		/*
+		 * PersonneDao personneDao = new PersonneDao(); 
+		 * VehiculeDao vehiculeDao = new VehiculeDao(); 
+		 * Vehicule vehicule = new Vehicule(1);
+		 * vehiculeDao.remove(vehicule);
+		 */
+		 
+		 //vehicule update :
+	       /* PersonneDao personneDao = new PersonneDao();
+			VehiculeDao  vehiculeDao = new VehiculeDao();
+			Vehicule vehicule = new Vehicule(2, "Ford", "Kuga", 2012, personneDao.findById(6));
+			vehiculeDao.update(vehicule);
+		}*/
+		
+		// vehicule findById:
+		/*
+		 * PersonneDao personneDao = new PersonneDao(); VehiculeDao vehiculeDao = new
+		 * VehiculeDao(); System.out.println(vehiculeDao.findById(2));
+		 */
+		
+		// vehicule findAll :
+		/*
+		 * PersonneDao personneDao = new PersonneDao(); VehiculeDao vehiculeDao = new
+		 * VehiculeDao(); System.out.println(vehiculeDao.findById(2)); for(Vehicule
+		 * vehicule: vehiculeDao.findAll()) { System.out.println(vehicule); }
+		 */
+		
+		// findByPersonne:
+		/*
+		 * PersonneDao personneDao = new PersonneDao(); VehiculeDao vehiculeDao = new
+		 * VehiculeDao(); for(Vehicule vehicule:
+		 * vehiculeDao.findByPersonne(personneDao.findById(5))) {
+		 * System.out.println(vehicule); }
+		 */
+		//PersonneService save
+		/*PersonneService personneService = new PersonneService();
+		Personne personne = new Personne("messi", "lionel");
+		System.out.println(personneService.save(personne));*/
+		
+		//PersonneService update
+		/*
+		 * PersonneService personneService = new PersonneService(); Personne personne =
+		 * new Personne(11, "paris", "lionel");
+		 * System.out.println(personneService.update(personne));
+		 */
+		
+		//PersonneService Remove:
+		PersonneService personneService = new PersonneService();
+		Personne personne = new Personne(800);
+		personneService.remove(personne);
 	}
+	
 }
+
+	
+		  
+		 
+	
